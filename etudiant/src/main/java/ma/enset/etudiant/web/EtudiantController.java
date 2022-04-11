8ed6cdc4a2e2e4f4e9bae8c93a7f4de6e61cbcc8
Tarik-OFKIR <tarikofkir2017@gmail.com>
2022-04-11 12:00:11 +0000

    Contriller

--- etudiant/src/main/java/ma/enset/etudiant/web/EtudiantController.java
+++ etudiant/src/main/java/ma/enset/etudiant/web/EtudiantController.java
@@ -1,14 +1,22 @@
 package ma.enset.etudiant.web;
 
 import lombok.AllArgsConstructor;
+import javax.validation.Valid;
 import ma.enset.etudiant.entites.Etudiant;
 import ma.enset.etudiant.repositories.EtudaintRepossitory;
+import ma.enset.etudiant.resourse.Genre;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -39,5 +47,44 @@ public class EtudiantController {
 
     @GetMapping( "/")
     public String home(){return "home";}
+
+    @GetMapping("/etudiant")
+    @ResponseBody
+    public List<Etudiant> Etudiant(){return  etudaintRepossitory.findAll();}
+
+    @GetMapping(path="/admin/formEtudiant")
+    public String formEtudiant(Model model) {
+
+        Etudiant etudiant=new Etudiant(null,"test","test","test",new Date(), Genre.MASCULIN,false);
+        model.addAttribute("etudiant",etudiant);
+        //model.addAttribute("patient",p);
+
+        return "formPatient";
+    }
+
+
+    @PostMapping(path="/amdin/save")
+    public String save(Model model, @Valid Etudiant etudiant, BindingResult bindingResult, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword) {
+
+        if(bindingResult.hasErrors()) {
+            return "formEtudiant";
+        }
+        etudaintRepossitory.save(etudiant);
+
+        return "redirect:/etudiant/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping(path="/admin/editEtudiant")
+    public String editPatient(Model model,Long id,int page,String keyword) {
+        Etudiant etudiant=etudaintRepossitory.findById(id).orElse(null);
+        if(etudiant==null) {
+            throw new RuntimeException("etudiant n'existe pas");
+        }
+        model.addAttribute("etudiant",etudiant);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+
+        return "editEtudinat";
+    }
 
 }
